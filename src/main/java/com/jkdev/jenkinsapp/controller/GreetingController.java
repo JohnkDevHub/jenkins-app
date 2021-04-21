b6ae7a9a83342d5d80f8449e65e33c0b28effc24
johnkgitlab <johnkatanga777@gmail.com>
2021-04-21 12:04:56 +0200

    alpha commit

---
+++ src/main/java/com/jkdev/jenkinsapp/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.jkdev.jenkinsapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class GreetingController {
+
+    @GetMapping("/greeting")
+
+    public String greet(){
+
+        return "Hello " ;//+ user.getEmail();
+
+    }
+
+}
